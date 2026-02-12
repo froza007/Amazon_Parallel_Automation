b95a85ce9dc562afe69fd565d5821cf6df198aa7
froza007 <barath282003@gmail.com>
2026-02-12 09:34:50 +0530

    add to cart bug and parallel execution fixed

--- src/main/java/pages/CartPage.java
+++ src/main/java/pages/CartPage.java
@@ -1,7 +1,13 @@
 package pages;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CartPage {
 
@@ -30,7 +36,15 @@ public class CartPage {
 
     public void addProductToCart(){
 
-        driver.findElement(addToCartBtn).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        WebElement addBtn = wait.until(
+            ExpectedConditions.elementToBeClickable(
+                By.xpath("(//input[@id='add-to-cart-button'])[2]")
+            )
+        );
+
+        addBtn.click();
     }
 
     public void openCart(){
